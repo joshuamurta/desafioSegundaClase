a81aa52759b8c6c83da7606f4ef646a12cf689d4
Joshua Guerreiro Murta <jguerreiro@bago.com.ar>
2021-11-12 13:22:01 -0300

    Print ArrayList<Heroes>.

--- joshuaguerreiromurta/dist/index.js
+++ joshuaguerreiromurta/dist/index.js
@@ -2,6 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var super_heroe_1 = require("./models/super_heroe");
 var spiderman = new super_heroe_1.SuperHeroe("Peter Parker", 33, false, true, false);
-console.log(spiderman.nombre);
-console.log(spiderman.edad);
-console.log(spiderman.superFuerza);
+var batman = new super_heroe_1.SuperHeroe("Bruno Diaz", 40, false, true, false);
+var superman = new super_heroe_1.SuperHeroe("Clark Kent", 80, false, true, true);
+var listaHeroes = [];
+listaHeroes.push(batman, superman, spiderman);
+listaHeroes.forEach(function (value) {
+    console.log("Nombre: " + value.nombre + '\n' + "Edad: " + value.edad + '\n' + "Poderes: " + (value.invisible == true ? 'Invisible ' : '') + (value.superFuerza == true ? 'Super Fuerza ' : '') + (value.volar == true ? 'Vuela' : ''));
+    console.log('\n');
+});
--- joshuaguerreiromurta/index.ts
+++ joshuaguerreiromurta/index.ts
@@ -1,6 +1,14 @@
 import {SuperHeroe} from './models/super_heroe';
 
 let spiderman = new SuperHeroe("Peter Parker",33,false,true,false);
-console.log(spiderman.nombre);
-console.log(spiderman.edad);
-console.log(spiderman.superFuerza);
+let batman = new SuperHeroe("Bruno Diaz",40,false,true,false);
+let superman = new SuperHeroe("Clark Kent",80,false,true,true);
+
+var listaHeroes: Array<SuperHeroe> = [];
+
+listaHeroes.push(batman,superman,spiderman);
+
+listaHeroes.forEach(function (value){
+    console.log("Nombre: " + value.nombre + '\n' + "Edad: " + value.edad + '\n' + "Poderes: " + (value.invisible == true ? 'Invisible ' : '') + (value.superFuerza == true ? 'Super Fuerza ' : '') + (value.volar == true ? 'Vuela' : ''));
+    console.log('\n');
+})
